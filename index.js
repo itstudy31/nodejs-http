1fb294005706cac60667a2d8285b2ef441a70e7a
itstudy31 <itstudy31@gmail.com>
2026-05-05 21:09:20 +0900

    feat:ログイン、ログアウト機能の実装

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-'use strict';
+﻿'use strict';
 const http = require('node:http');
 const pug = require('pug');
+const auth = require('http-auth');
+
+const basic = auth.basic(
+  { realm: 'Enquetes Area.' },
+  (username, password, callback) => {
+    callback(username === 'guest' && password === 'xaXZJQmE');
+  }
+);
+
 const server = http
-  .createServer((req, res) => {
+  .createServer(basic.check((req, res) => {
     const now = new Date();
     console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    
+    if (req.url === '/logout') {
+      res.writeHead(401, {
+        'Content-Type': 'text/plain; charset=utf-8'
+      });
+      res.end('ログアウトしました');
+      return;
+    }
+
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -19,7 +37,7 @@ const server = http
         } else if (req.url === '/enquetes') {
           res.write('<!DOCTYPE html><html lang="ja"><body>' +
             '<h1>アンケート一覧</h1><ul>' +
-            '<li><a href="/enquetes/yaki-tofu">焼き肉・湯豆腐</a></li>' +
+            '<li><a href="/enquetes/yaki-tofu">焼き豆腐・湯豆腐</a></li>' +
             '<li><a href="/enquetes/rice-bread">ごはん・パン</a></li>' +
             '<li><a href="/enquetes/sushi-pizza">寿司・ピザ</a></li>' +
             '</ul></body></html>');
@@ -42,7 +60,7 @@ const server = http
         }
         res.end();
         break;
-      case 'POST':
+      case 'POST': {
         let rawData = '';
         req
           .on('data', chunk => {
@@ -50,7 +68,7 @@ const server = http
           })
           .on('end', () => {
             const answer = new URLSearchParams(rawData);
-            const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
+            const body = `${answer.get('name')}さんは${answer.get('favorite')}を選択しました`;
             console.info(`[${now}] ${body}`);
             res.write(
               `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
@@ -58,16 +76,20 @@ const server = http
             res.end();
           });
         break;
+      }
       default:
+        // res.statusCode = 404;
+        // res.end('Not Found');
         break;
     }
-  })
+  }))
   .on('error', e => {
     console.error(`[${new Date()}] Server Error`, e);
   })
   .on('clientError', e => {
     console.error(`[${new Date()}] Client Error`, e);
   });
+
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.info(`[${new Date()}] Listening on ${port}`);
